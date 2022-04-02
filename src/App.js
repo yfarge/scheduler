1b202619986b0ea341d191f60e386c14a24c81a0
Yousef Farge <yousef.farge@gmail.com>
2022-04-02 09:48:59 -0500

    Style the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -41,19 +42,22 @@ const getCourseNumber = course => (
 );
 
 const Course = ({ course }) => (
-  <div>
-    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+  <div className="card m-1 p-2">
+    <div className="card-body">
+      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+      <div className="card-text">{ course.title }</div>
+    </div>
   </div>
 );
 
 const CourseList = ({ courses }) => (
-  <div>
+  <div className="course-list">
   { Object.values(courses).map(course => <Course key={ course.id} course={ course } />) }
   </div>
 );
 
 const App = () =>  (
-  <div>
+  <div className="container">
     <Banner title={ schedule.title } />
     <CourseList courses={ schedule.courses } />
   </div>
