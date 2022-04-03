8611a54e5d0e4c9a943cf09cda75d5b6e13018d3
yfarge <83893088+yfarge@users.noreply.github.com>
2022-04-03 14:53:26 -0500

    added filtering by term

--- src/App.js
+++ src/App.js
@@ -25,11 +25,38 @@ const Course = ({ course }) => (
   </div>
 );
 
-const CourseList = ({ courses }) => (
-  <div className="course-list">
-  { Object.values(courses).map(course => <Course key={ course.id} course={ course } />) }
+const TermButton = ({term, setTerm, checked}) => (
+  <>
+    <input type="radio" id={term} className="btn-check" autoComplete="off" checked={checked}
+      onChange={() => setTerm(term)} />
+    <label className="btn btn-success m-1 p-2" htmlFor={term}>
+    {term}
+    </label>
+  </>
+);
+
+const TermSelector = ({term, setTerm}) => (
+  <div className="btn-group">
+    {
+      Object.values(terms).map(value => (
+        <TermButton key={value} term={value} setTerm={setTerm} checked={value === term}/>
+      ))
+    }
   </div>
 );
+
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = useState('Fall');
+  const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+  return(
+    <>
+      <TermSelector term={term} setTerm={setTerm} />
+      <div className="course-list">
+      { termCourses.map(course => <Course key={ course.id} course={ course } />) }
+      </div>
+    </>
+  );
+};
 
 const App = () => {
   const [schedule, setSchedule] = useState();
